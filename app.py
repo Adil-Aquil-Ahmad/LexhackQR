290e1b4156875365fca485c0d10cb8fb43546dfc
Adil Aquil Ahmad <adilaquil2005@gmail.com>
2025-04-17 21:28:06 +0530

    fixed

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def get_scanned_id():
     return jsonify({"scanned_id": scanned_id, "status": scanned_status})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
